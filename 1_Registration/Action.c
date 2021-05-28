32054094e438aa17504e8ccf372d4e743ac5e98c
Anton Nemtsev <nemtsev.online@gmail.com>
2021-05-28 21:18:24 +0300

    Add files

---
+++ 1_Registration/Action.c
@@ -0,0 +1,166 @@
+Action()
+{
+	lr_start_transaction("Registration");
+	
+	
+	web_reg_find("Text=Web Tours",
+		LAST);
+
+	lr_start_transaction("Web_tours");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("sec-ch-ua", 
+		"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"");
+
+	web_add_auto_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	/*Possible OAUTH authorization was detected. It is recommended to correlate the authorization parameters.*/
+
+	lr_end_transaction("Web_tours",LR_AUTO);
+	
+	web_reg_find("Text=User Information",
+		LAST);
+
+	lr_start_transaction("Sign_up");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(6);
+
+	web_url("sign up now", 
+		"URL=http://localhost:1080/cgi-bin/login.pl?username=&password=&getInfo=true", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/home.html", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Sign_up",LR_AUTO);
+	
+	web_reg_find("Text=User Information",
+		LAST);
+
+	lr_start_transaction("Complete_form");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+	
+	
+	web_submit_data("login.pl", 
+		"Action=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=info", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl?username=&password=&getInfo=true", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=username", "Value={login}", ENDITEM, 
+		"Name=password", "Value={password}", ENDITEM, 
+		"Name=passwordConfirm", "Value={password}", ENDITEM, 
+		"Name=firstName", "Value={name}", ENDITEM, 
+		"Name=lastName", "Value={last}", ENDITEM, 
+		"Name=address1", "Value={address}", ENDITEM, 
+		"Name=address2", "Value={city}", ENDITEM, 
+		"Name=register.x", "Value=63", ENDITEM, 
+		"Name=register.y", "Value=10", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Complete_form",LR_AUTO);
+	
+	web_reg_find("Text=User has returned to the home page",
+		LAST);
+
+	lr_start_transaction("Login_after_registration");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	lr_think_time(80);
+
+	web_url("button_next.gif", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=menus", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Login_after_registration",LR_AUTO);
+	
+	web_reg_find("Text=Web Tours",
+		LAST);
+
+	lr_start_transaction("Log_out");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(37);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+
+	lr_end_transaction("Log_out",LR_AUTO);
+	
+	lr_end_transaction("Registration", LR_AUTO);
+
+	return 0;
+}
