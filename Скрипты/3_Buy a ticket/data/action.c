e9fd194c07833a2f700c1422d0b284d97630fc8d
Anton Nemtsev <nemtsev.online@gmail.com>
2021-06-04 18:39:51 +0300

    Updated scripts

---
+++ Скрипты/3_Buy a ticket/data/action.c
@@ -0,0 +1,223 @@
+Action()
+{
+
+	lr_start_transaction("Web_tours");
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("sec-ch-ua", 
+		"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"");
+
+	web_add_auto_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Web_tours",LR_AUTO);
+
+	lr_start_transaction("Login");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(32);
+
+	web_submit_data("login.pl", 
+		"Action=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=body", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=131405.135358445zHfQVfVpzftAfftpQAQQ", ENDITEM, 
+		"Name=username", "Value=Jo", ENDITEM, 
+		"Name=password", "Value=123", ENDITEM, 
+		"Name=login.x", "Value=58", ENDITEM, 
+		"Name=login.y", "Value=12", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Login",LR_AUTO);
+
+	lr_start_transaction("Flights");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(35);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Flights",LR_AUTO);
+
+	lr_start_transaction("Find_flight");
+
+	web_add_auto_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(52);
+
+	web_submit_data("reservations.pl", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value=Frankfurt", ENDITEM, 
+		"Name=departDate", "Value=05/30/2021", ENDITEM, 
+		"Name=arrive", "Value=London", ENDITEM, 
+		"Name=returnDate", "Value=06/30/2021", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=findFlights.x", "Value=48", ENDITEM, 
+		"Name=findFlights.y", "Value=7", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Find_flight",LR_AUTO);
+
+	lr_think_time(48);
+
+	lr_start_transaction("Choice");
+
+	web_submit_data("reservations.pl_2", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=outboundFlight", "Value=120;266;05/30/2021", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=reserveFlights.x", "Value=34", ENDITEM, 
+		"Name=reserveFlights.y", "Value=9", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Choice",LR_AUTO);
+
+	lr_start_transaction("Payment_details");
+
+	web_revert_auto_header("Origin");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(34);
+
+	web_submit_data("reservations.pl_3", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=firstName", "Value=Jo", ENDITEM, 
+		"Name=lastName", "Value=Jo", ENDITEM, 
+		"Name=address1", "Value=Jo", ENDITEM, 
+		"Name=address2", "Value=Jo", ENDITEM, 
+		"Name=pass1", "Value=Jo Jo", ENDITEM, 
+		"Name=creditCard", "Value=123456789", ENDITEM, 
+		"Name=expDate", "Value=1234", ENDITEM, 
+		"Name=oldCCOption", "Value=", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=outboundFlight", "Value=120;266;05/30/2021", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=returnFlight", "Value=", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=buyFlights.x", "Value=16", ENDITEM, 
+		"Name=buyFlights.y", "Value=7", ENDITEM, 
+		"Name=.cgifields", "Value=saveCC", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Payment_details",LR_AUTO);
+
+	lr_start_transaction("Log_out");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(38);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Log_out",LR_AUTO);
+
+	return 0;
+}
---
+++ Скрипты/5_Deleting ticket/Action.c
@@ -0,0 +1,240 @@
+Action()
+{
+	
+	
+	int flight_count_before = 0;
+	int flight_count_after = 0;
+	int i;
+	int j;
+	
+	lr_start_transaction("5_Deleting_ticket");
+
+	lr_start_transaction("Web_tours");
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("sec-ch-ua", 
+		"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"");
+
+	web_add_auto_header("sec-ch-ua-mobile", 
+		"?0");
+
+/*Correlation comment - Do not change!  Original value='131406.335358445zHfQcVDpDHftAfffpciAcf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+		
+	web_reg_find("Text=Web Tours",
+        LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Web_tours",LR_AUTO);
+
+	lr_start_transaction("Login");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(22);
+	
+	web_reg_find("Text=User password was correct",
+        LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={login}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=60", ENDITEM,
+		"Name=login.y", "Value=8", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	lr_end_transaction("Login",LR_AUTO);
+	
+	lr_start_transaction("Itinerary");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(42);
+
+/*Correlation comment - Do not change!  Original value='251444829-798-JJ' Name ='flightID' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=flightID",
+		"TagName=input",
+		"Extract=value",
+		"Name=flightID",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/itinerary.pl*",
+		LAST);
+		
+		web_reg_save_param("flight_ID_origin",
+		"LB=name=\"flightID\" value=\"",
+		"RB=-",
+		"Ord=ALL",
+		LAST);
+		
+	web_reg_find("Text= User wants the intineraries",
+		LAST);
+
+
+	web_url("Itinerary Button",
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary",
+		"TargetFrame=body",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home",
+		"Snapshot=t3.inf",
+		"Mode=HTML",
+		LAST);
+
+	lr_end_transaction("Itinerary",LR_AUTO);
+
+	lr_start_transaction("Cancel");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_header("sec-ch-ua", 
+		"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"");
+
+	web_add_header("sec-ch-ua-mobile", 
+		"?0");
+
+	lr_think_time(32);
+	
+//	web_reg_save_param("flight_ID",
+//		"LB=name=\"flightID\" value=\"",
+//		"RB=-",
+//		"Ord=ALL",
+//		LAST);
+	
+	web_reg_find("Text=Flights List",
+		LAST);
+	
+	web_reg_find("Fail=Found",
+	             "Text=Flights_ID_origin_1})",
+		LAST);
+
+	web_submit_form("itinerary.pl", 
+		"Snapshot=t16.inf", 
+		ITEMDATA, 
+		"Name=1", "Value=on", ENDITEM, 
+		"Name=removeFlights.x", "Value=65", ENDITEM, 
+		"Name=removeFlights.y", "Value=9", ENDITEM, 
+		LAST);
+	
+
+//	flight_count_before = atoi(lr_eval_string("{flight_ID_origin_count}"));
+//	flight_count_after = atoi(lr_eval_string("{flight_ID_count}"));
+//	lr_message("Броней до: %i, Броней после: %i", flight_count_before, flight_count_after);
+//
+//	for (j=0; j < flight_count_before; j++){
+//	           	i = strcmp(lr_eval_string("{flight_ID_origin_1}"), lr_paramarr_idx("{flight_ID}", j));
+//		if (i != 0){
+//	           		lr_end_transaction("Cancel",LR_FAIL);
+//	           	}}
+	
+	lr_end_transaction("Cancel",LR_AUTO);
+		
+	
+	lr_message("До удаления - %s", lr_eval_string("{flight_ID_origin_count}"));
+	lr_message("После удаления - %s", lr_eval_string("{flight_ID_count}"));
+
+	lr_start_transaction("Log_out");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	lr_think_time(30);
+
+	web_reg_find("Text=Web Tours",
+        LAST);
+	
+	web_url("SignOff Button",
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1",
+		"TargetFrame=body",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary",
+		"Snapshot=t5.inf",
+		"Mode=HTML",
+		LAST);
+
+	lr_end_transaction("Log_out",LR_AUTO);
+	
+	lr_end_transaction("5_Deleting_ticket", LR_AUTO);
+
+	return 0;
+}
